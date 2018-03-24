1baa59bfda5a1194e304ecb1b718870b1fb95398
Ajay Tripathi <ajay39in@gmail.com>
2018-03-24 20:46:41 +0530

    Automatically Generate Group 'admin'

--- app/portal/admin.py
+++ app/portal/admin.py
@@ -1,12 +1,30 @@
-from django.contrib import admin
+from django.contrib import admin as administrator
 from .models import (
     foss,
     tutorial_detail,
     payment
 )
+from django.contrib.auth.models import (
+    Group,
+    Permission
+)
+from django.contrib.contenttypes.models import ContentType
 
 # Register your models here.
 
-admin.site.register(foss)
-admin.site.register(tutorial_detail)
-admin.site.register(payment)
+administrator.site.register(foss)
+administrator.site.register(tutorial_detail)
+administrator.site.register(payment)
+
+
+# Registration of Groups
+
+admin, created = Group.objects.get_or_create(name='admin')
+# Code to add permission to group
+fossModel = ContentType.objects.get(model='foss').pk
+TutorialModel = ContentType.objects.get(model='tutorial_detail').pk
+PaymentModel = ContentType.objects.get(model='payment').pk
+allPermissions = Permission.objects.filter(content_type_id__in=[
+    fossModel, TutorialModel, PaymentModel])
+for permission in allPermissions:
+    admin.permissions.add(permission)
